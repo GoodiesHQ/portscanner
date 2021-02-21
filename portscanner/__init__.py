b0c97f9e0cd955bb33e28207957985862a9e2e14
GoodiesHQ <aarcher73k@gmail.com>
2021-02-21 03:58:12 -0800

    First beta release

--- portscanner/__init__.py
+++ portscanner/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '0.1.0'
+from portscanner.scanner import PortScanner
+from portscanner.types import ScanState, ScanInfo
+
+__version__ = '0.5.0'
--- portscanner/parsing.py
+++ portscanner/parsing.py
@@ -1,4 +1,15 @@
+"""
+Parsing functions used throughout portscanner
+"""
+from contextlib import suppress
+from functools import partial
+
 from portscanner.utils import trycast
+
+
+__all__ = [
+    "port_range",
+]
 
 
 def port_range(port_ranges: str, quiet: bool = True):
@@ -9,19 +20,19 @@ def port_range(port_ranges: str, quiet: bool = True):
         21-23,80,443,8000-8080
     """
     valid_ports = set()
-    for port_range in map(str.strip, port_ranges.split(",")):
+    for port_range in port_ranges.split(","):
         try:
             if "-" not in port_range:
                 if (port := trycast(int, port_range)) is not None:
                     if 0 < port < 65536:
                         valid_ports.add(port)
             elif port_range.count("-") == 1:
-                start, stop = map(str.strip,port_range.split("-")
-                if (start := trycast(int, start)) is None or \
-                    (stop := trycast(int, stop)) is None:
+                start, stop = map(
+                    lambda s: trycast(int, s.strip()), port_range.split("-")
+                )
+                if start is None or stop is None:
                     raise ValueError
                 start, stop = (stop, start) if start > stop else (start, stop)
-                print(start, stop)
                 for i in range(max((0, start)), min((stop, 65535)) + 1):
                     valid_ports.add(i)
             else:
