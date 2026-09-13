790782ce1ad0595348e62fc1978b41d36aa7509a
Austin Archer <austinarcher@Austins-MacBook-Air.local>
2026-09-13 13:20:21 -0700

    Version bump

--- portscanner/__init__.py
+++ portscanner/__init__.py
@@ -1,4 +1,10 @@
 from portscanner.scanner import PortScanner
 from portscanner.types import ScanState, ScanInfo
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
+
+__all__ = [
+    "PortScanner",
+    "ScanState",
+    "ScanInfo",
+]
